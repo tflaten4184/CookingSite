63a08f5cabfa02e418f1bf2e61a7ff61ccb3285f
tflaten4184 <tflaten4184@gmail.com>
2023-04-14 12:26:54 -0700

    setup media and thumbnails

--- recipes/models.py
+++ recipes/models.py
@@ -3,6 +3,7 @@ from django.db import models
 class Recipe(models.Model):
 
     name = models.CharField(max_length=100)
+    image = models.ImageField()
 
     # For later, other fields:
     # Image?
