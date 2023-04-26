592d62d9184f8d4d59ef9cddf401b2d3e81f6294
tflaten4184 <tflaten4184@gmail.com>
2023-04-25 17:47:47 -0700

    Navbar text to match logo text

--- cookingsite/settings.py
+++ cookingsite/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static') # used for production
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static') # used for production
 STATICFILES_DIRS = (
         os.path.join(BASE_DIR, "static"),
     ) # used for development
