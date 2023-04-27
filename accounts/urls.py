35546f408212c161fc6a2a2c45b19690b2bf8f89
tflaten4184 <tflaten4184@gmail.com>
2023-04-26 18:06:05 -0700

    cleanup comments and placeholders

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,9 +5,6 @@ from .views import SignUpView
 app_name = "accounts"
 
 urlpatterns = [
-    # path("", include("django.contrib.auth.urls"))
-    # path('login/', auth_views.LoginView.as_view()),
     path('signup/', SignUpView.as_view(), name="signup"),
     path('', include("django.contrib.auth.urls")),
-
 ]
