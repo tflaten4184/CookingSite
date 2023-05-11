f26606c3a75b0935f1de4483ba74de194a588563
tflaten4184 <tflaten4184@gmail.com>
2023-05-11 14:14:38 -0700

    fix: signup form includes email address

--- accounts/views.py
+++ accounts/views.py
@@ -3,7 +3,9 @@ from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views import generic
 
+from .forms import CreateUserForm
+
 class SignUpView(generic.CreateView):
-    form_class = UserCreationForm
+    form_class = CreateUserForm
     success_url = reverse_lazy("accounts:login")
     template_name = "registration/signup.html"
