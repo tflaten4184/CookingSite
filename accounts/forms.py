5ac2fa3194c05dfcb6388f96c3290c05586eeb5f
tflaten4184 <tflaten4184@gmail.com>
2023-05-11 17:44:24 -0700

    prevent signup with duplicate email

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,8 +1,11 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.models import User
+# from django.contrib.auth.models import User
+from .models import User
 
 class CreateUserForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+
     class Meta:
         model = User
         fields = [
@@ -11,3 +14,9 @@ class CreateUserForm(UserCreationForm):
             'password1',
             'password2',
         ]
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        if User.objects.filter(email=email).exists():
+            raise forms.ValidationError("This email is already taken.")
+        return email
--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext as _
+from django.contrib.auth.models import AbstractUser, Group, Permission
 
-# Create your models here.
+class User(AbstractUser):
+    email = models.EmailField(_('email address'), unique=True)
+    groups = models.ManyToManyField(
+        Group,
+        verbose_name=_('groups'),
+        blank=True,
+        help_text=_('The groups this user belongs to.'),
+        related_name='%(app_label)s_%(class)s_groups',  # Add a related name
+        related_query_name='%(app_label)s_%(class)s',
+    )
+    user_permissions = models.ManyToManyField(
+        Permission,
+        verbose_name=_('user permissions'),
+        blank=True,
+        help_text=_('Specific permissions for this user.'),
+        related_name='%(app_label)s_%(class)s_user_permissions',  # Add a related name
+        related_query_name='%(app_label)s_%(class)s',
+    )
