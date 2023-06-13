3ac59fae506779b4c97cb5b0bd21cf320ff24258
tflaten4184 <tflaten4184@gmail.com>
2023-06-13 14:49:02 -0700

    login API, install JWT

--- api/urls.py
+++ api/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('', views.getRoutes, name="routes"),
     path('recipes/', views.getRecipes, name="recipes"),
     path('register', views.RegisterView.as_view(), name="register"),
+    path('login', views.LoginView.as_view(), name="login"),
 ]
--- api/views.py
+++ api/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from rest_framework.decorators import api_view, APIView
 from rest_framework.response import Response
+from rest_framework.exceptions import AuthenticationFailed
 
 from recipes.models import Recipe
+from accounts.models import User
 from .serializers import UserSerializer, RecipeSerializer
 
 @api_view(['GET'])
@@ -25,3 +27,21 @@ class RegisterView(APIView):
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data)
+    
+class LoginView(APIView):
+    def post(self, request):
+        email = request.data['email']
+        password = request.data['password']
+
+        user = User.objects.filter(email=email).first()
+
+        if user is None:
+            raise AuthenticationFailed('User not found!')
+        
+        if not user.check_password(password):
+            raise AuthenticationFailed('Incorrect password!')
+        
+        return Response({
+            'message': 'success'
+        })
+        
